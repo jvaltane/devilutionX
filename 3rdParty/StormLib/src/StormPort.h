ded704cfdcca068a4021ff9dda4fe5033feeb979
Joni Valtanen <jvaltane@kapsi.fi>
2020-03-31 06:21:08 +0300

    MorphOS: use real stat64 etc,

--- 3rdParty/StormLib/src/StormPort.h
+++ 3rdParty/StormLib/src/StormPort.h
@@ -297,7 +297,7 @@
 #endif // !PLATFORM_WINDOWS
 
 // 64-bit calls are supplied by "normal" calls on Mac
-#if defined(PLATFORM_MAC) || defined(PLATFORM_HAIKU) || defined(PLATFORM_AMIGA) || defined(PLATFORM_SWITCH) || defined(PLATFORM_MORPHOS)
+#if defined(PLATFORM_MAC) || defined(PLATFORM_HAIKU) || defined(PLATFORM_AMIGA) || defined(PLATFORM_SWITCH)
 
   #define stat64  stat
   #define fstat64 fstat
